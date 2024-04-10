8a201b59a7e3117d2768b3b11cdbdfe219129fd1
iamsohust <iamsohust@163.com>
2024-04-10 10:59:43 +0800

    2024-4-40-1059

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,7 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
+
 
 pub enum Command {
     Uppercase,
@@ -32,11 +32,38 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            /*
+            if(command==Command::Uppercase){
+                string.to_uppercase();
+            }else if(command==Command::Trim){
+                string.trim().to_string();
+            }else{
+                let x=command.0;
+                for 0..x{
+                    string=string.to_owned()+"bar";
+                }
+            }
+            */
+            match command {
+                Command::Uppercase => {
+                    output.push(string.to_uppercase());
+                },
+                Command::Trim => {
+                    output.push(string.trim().to_string());
+                },
+                Command::Append(times) => {
+                    let mut new_string = string.to_owned();
+                    for _ in 0..*times {
+                        new_string += "bar";
+                    }
+                    output.push(new_string);
+                },
+            }
         }
         output
     }
@@ -45,12 +72,12 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module;
     use super::Command;
 
     #[test]
     fn it_works() {
-        let output = transformer(vec![
+        let output = my_module::transformer(vec![
             ("hello".into(), Command::Uppercase),
             (" all roads lead to rome! ".into(), Command::Trim),
             ("foo".into(), Command::Append(1)),
