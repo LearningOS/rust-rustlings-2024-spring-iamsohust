55dfa0bb3e7c21b3d21a061f690b73bf0fd254e2
iamsohust <iamsohust@163.com>
2024-05-05 22:34:50 +0800

    .

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -89,21 +89,3 @@ mod tests {
         assert!(timestamp >= e && timestamp < e + 10);
     }
 }
-
-fn main() {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_success() {
-        let timestamp = std::time::SystemTime::now()
-            .duration_since(std::time::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-        let s = std::env::var("TEST_FOO").unwrap();
-        let e: u64 = s.parse().unwrap();
-        assert!(timestamp >= e && timestamp < e + 10);
-    }
-}
